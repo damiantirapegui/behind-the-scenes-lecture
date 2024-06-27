15d289c4bf1b3349d7d90abf9d4e1bd30b7effc2
damiantirapegui <damiantirapegui@gmail.com>
2024-06-27 21:29:36 +0200

    continuing

--- script.js
+++ script.js
@@ -4,16 +4,24 @@ function calcAge(birthyear) {
   const age = 2037 - birthyear;
 
   function pringAge() {
-    const outPut = `${firstName}, you are ${age}, born in ${birthyear}`;
+    let outPut = `${firstName}, you are ${age}, born in ${birthyear}`;
     console.log(outPut);
 
     if (birthyear >= 1981 && birthyear <= 1996) {
       var millenial = true;
+      //creating new variable with same name as outer scope's variable
+      const firstName = 'Jonas';
+
+      //reassigning outer scope's variaböe
+      outPut = 'NEW OUTPUT';
       const str = `Oh, and you are a millenial, ${firstName}`;
       console.log(str);
+      function add(a, b) {
+        return a + b;
+      }
+      add(2, 3);
     }
-    // console.log(str);
-    // console.log(millenial);
+    console.log(outPut);
   }
   pringAge();
 
